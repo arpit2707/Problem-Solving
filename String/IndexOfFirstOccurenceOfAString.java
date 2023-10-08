7ba62f97b66ad9f6a13311e3a37988e9db8394ad
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-10-08 23:13:12 +0530

    #string

---
+++ String/IndexOfFirstOccurenceOfAString.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        boolean isSubstring = true;
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            String s = haystack.substring(i, i + needle.length());
+            if (s.equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
