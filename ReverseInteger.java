2e2a4f22e1c01fccc4d82e5b6475adaa3ebf03bb
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-10-10 23:23:58 +0530

    #integerMedium

---
+++ ReverseInteger.java
@@ -0,0 +1,17 @@
+class ReverseInteger {
+    public int reverse(int x) {
+        int temp=x;
+        String s=x+"";
+    long r=0;
+       while(x!=0){
+           long digit=x%10;
+           r=r*10+digit;
+           x=x/10;
+                 if (r > Integer.MAX_VALUE || r < Integer.MIN_VALUE) {
+        return 0;
+    }
+       }
+  
+    return (int)(r);
+    }
+}
