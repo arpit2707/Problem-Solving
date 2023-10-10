ac0caddc081b52e1262f640a91ff302aad1d4526
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-10-10 23:25:31 +0530

    #integer

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,17 +1,16 @@
 class ReverseInteger {
     public int reverse(int x) {
-        int temp=x;
-        String s=x+"";
-    long r=0;
-       while(x!=0){
-           long digit=x%10;
-           r=r*10+digit;
-           x=x/10;
-                 if (r > Integer.MAX_VALUE || r < Integer.MIN_VALUE) {
-        return 0;
-    }
-       }
-  
-    return (int)(r);
+        int temp = x;
+        String s = x + "";
+        long r = 0;
+        while (x != 0) {
+            long digit = x % 10;
+            r = r * 10 + digit;
+            x = x / 10;
+            if (r > Integer.MAX_VALUE || r < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return (int) (r);
     }
 }
